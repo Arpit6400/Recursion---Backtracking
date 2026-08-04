81aeae3d8d9609552e5dfa0da3430c2382488c1c
Arpit Hamjade <arpitshirishhamjade@gmail.com>
2026-08-04 20:58:19 +0530

    Optimized

--- rec23.java
+++ rec23.java
@@ -4,22 +4,24 @@ public class rec23 {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        solve(0, n, sb, ans);
+        solve(0, n, sb, ans, 0, 0);
         return ans;
     }
-    private void solve(int idx, int n, StringBuilder sb, List<String> ans) {
+    private void solve(int idx, int n, StringBuilder sb, List<String> ans, int open, int close) {
      if (idx == 2*n) {
-        if (isValid(sb.toString())) {
-            ans.add(sb.toString());
-        }
+        ans.add(sb.toString()); 
         return;
      }
+     if (open < n) {
      sb.append('(');
-     solve(idx+1, n, sb, ans);
+     solve(idx+1, n, sb, ans, open+1, close);
      sb.deleteCharAt(sb.length()-1);
+     }
+     if (close < open) {
      sb.append(')');
-     solve(idx+1, n, sb, ans);
+     solve(idx+1, n, sb, ans, open, close + 1);
      sb.deleteCharAt(sb.length() -1);
+     }
     }
     private boolean isValid(String str) {
         Stack<Character> stk = new Stack<>();
